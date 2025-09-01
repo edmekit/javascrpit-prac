7de5ea89cbcbdd686bbeef5ec48fcc6448a09ec6
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-09-01 16:19:20 +0800

    true

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-let cart = 0
-
-function add_show(cart) {
-    console.log(`Cart has ${cart} items`)
+function pick (choice){
+    num = Math.random()
+    if (num <= 0.3){
+        computer = "Rock"
+    }
+    else if (num <= 0.6){
+        computer = "Paper"
+    }
+    else {
+        computer = "Scissors"
+    }
+    if (choice === user){
+        alert(`You picked ${choice}. The computer picked ${computer}, it is a tie`)
+    }
+    else {
+        if (choice === "Rock") and (computer === "Paper")
+        {
+            alert(`You picked ${choice}. The computer picked ${computer}, you lose`)
+        }
+        if (choice === "Paper") and (computer === "Scissors")
+        {
+            alert(`You picked ${choice}. The computer picked ${computer}, you lose`)
+        }
+        if (choice === "Scissors") and (computer === "Rock")
+        {
+            alert(`You picked ${choice}. The computer picked ${computer}, you lose`)
+        }
+    }
 }
